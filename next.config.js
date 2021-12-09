c74806469c83de7019012cff94a18c29a22cb43c
Ahmed Samir <ahmedsamir551@gmail.com>
2021-12-10 01:06:54 +0200

    env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
-        NEXT_PUBLIC_API_URL:process.env.NEXT_PUBLIC_API_URL,
-        NEXT_PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN
+        API_URL:process.env.NEXT_PUBLIC_API_URL,
+        ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN
     },
     images: {
         domains: ['cdn.shopify.com']
